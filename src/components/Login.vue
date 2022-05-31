84c1665af5628cee3919ab736bc29863c3e56553
艺函 柴 <2366791707@qq.com>
2022-05-31 22:56:56 +0800

    updated button.vue and login.vue, can simplely login, next should restore login statue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,42 +1,108 @@
 <script>
+import Button from './forms/Button.vue'
+
 export default {
     data(){
         return{
             logStatu: 'not logged in',
-            user: '',
+            username: '',
+            password: '',
             logging: 0,
+            checkurl: 'http://192.168.1.112:8081/user/check',
+            regisurl: 'http://192.168.1.112:8081/register',
+            constLogin: 'Login',
         }
     },
+    components: {
+        Button,
+    },
     methods: {
-        alterX: function(s){
-            return 1-s;
-        }
+        alterLogin: function(){
+            this.logging = 1-this.logging;
+        },
+        Login: function(){
+            console.log(this.username, this.password);
+            var data = {
+                    "username" : this.username,
+                    "password" : this.password,
+            }
+            fetch(this.checkurl,{
+                body: JSON.stringify(data), // must match 'Content-Type' header
+                cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+                credentials: 'same-origin', // include, same-origin, *omit
+                headers: {
+                'user-agent': 'Mozilla/4.0 MDN Example',
+                'content-type': 'application/json'
+                },
+                method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                mode: 'cors', // no-cors, cors, *same-origin
+                redirect: 'follow', // manual, *follow, error
+                referrer: 'no-referrer', // *client, no-referrer
+            }).then(response => response.json()).then(res => {
+                console.log(res);
+                if(res.res==='True'){
+                    // this.logStatu = 'logged in';
+                    console.log('log successfully,', this.username);
+                    this.alterLogin();
+                    this.logStatu = 'logged in';
+                }
+            });
+        },
+        Register: function(){
+            console.log(this.username, this.password);
+            var data = {
+                    "username" : this.username,
+                    "password" : this.password,
+            }
+            fetch(this.regisurl,{
+                body: JSON.stringify(data), // must match 'Content-Type' header
+                cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+                credentials: 'same-origin', // include, same-origin, *omit
+                headers: {
+                'user-agent': 'Mozilla/4.0 MDN Example',
+                'content-type': 'application/json'
+                },
+                method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                mode: 'cors', // no-cors, cors, *same-origin
+                redirect: 'follow', // manual, *follow, error
+                referrer: 'no-referrer', // *client, no-referrer
+            }).then(response => response.json()).then(res => {
+                console.log(res);
+            });
+        },
+        Update: function(){
+            this.username =  document.getElementById('nickname').value;
+            this.password = document.getElementById('password').value;
+        },
+        Logout: function(){
+            this.logStatu = 'not logged in';
+            this.alterLogin();
+        },
     }
-    // props: ['logStatu', 'login', 'logout']
 }
-// logStatu='not logged in';
 </script>
 
 <template>
-    <div v-on:click="logging=alterX(logging)">
+    <div v-on:click="alterLogin()">
         <div v-if=" logStatu === 'not logged in' ">
-            <div class="px-6 py-2 text-sm font-semibold uppercase rounded-sm text-white transition bg-gradient-to-r from-purple-500 to-blue-500">
-                Log in
-            </div>
+            <Button :text="'login'"></Button>
         </div>
         <div v-else>
-            <div class="px-6 py-2 text-sm font-semibold uppercase rounded-sm text-white transition bg-gradient-to-r from-purple-500 to-blue-500">
-                Log out
-            </div>
+            <Button :text="this.username" ></Button>
         </div>
     </div>
-    <div v-if="logging === 1" class="fixed top-1/2 left-1/2 -mx-48 -my-48 w-96 h-60 border-solid border-8 rounded-lg border-blue-300 bg-blue-100">
+    <div v-if="logging === 1 && logStatu === 'not logged in' " class="fixed top-1/2 left-1/2 -mx-48 -my-48 w-96 h-60 border-solid border-8 rounded-lg border-blue-300 bg-blue-100">
         <div class="border-none border-2 border-black w-56 mx-auto my-8">
-            nickname: <input type="text" class="w-32" /><br/><br/>
-            passowrd: <input type="password" class="w-32" /><br/>
+            nickname: <input id="nickname" type="text" v-on:change="Update()" class="w-32" /><br/><br/>
+            passowrd: <input id='password' type="password" v-on:change="Update()" class="w-32" /><br/>
         </div>
         <div>
-            <button class="border-solid border-2 border-black" >Login</button>
+            <Button :text="'login'" v-on:click="Login()" class="mx-24 my-12"></Button>
+            <Button :text="'register'" v-on:click="Register()" class="mx-48 -my-20"></Button>
         </div>
+    </div>
+    <div v-if="logging === 1 && logStatu === 'logged in' " class="fixed top-1/2 left-1/2 -mx-48 -my-48 w-96 h-60 border-solid border-8 rounded-lg border-blue-300 bg-blue-100" >
+        <p>确定登出？</p>
+        <Button :text="'log out'" v-on:click="Logout()" ></Button>
     </div>
 </template>
--- src/components/forms/Button.vue
+++ src/components/forms/Button.vue
@@ -1,14 +1,13 @@
-<script setup>
-import { defineProps } from 'vue';
-const props = defineProps({
-  text: { type: String, required: true }
-});
+<script>
+export default {
+    props: ['text']
+}
 </script>
 
 <template>
   <div>
     <button
       class="px-6 py-2 text-sm font-semibold uppercase rounded-sm text-white transition bg-gradient-to-r from-purple-500 to-blue-500"
-    >{{ props.text }}</button>
+    >{{ text }}</button>
   </div>
 </template>
